ec885e0ea18d856b7ff18d2d3b3b55311ec4f5d5
Marina Novosad <zmeika.marina@gmail.com>
2025-02-19 12:21:46 +0000

    Square

--- chess.cpp
+++ chess.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 const short BLACK = 1;
 const short WHITE = 0;
+const short BOARD_SIZE = 8;
 string COLOR[2] = {"white", "black"};
 
 class Piece 
@@ -117,31 +118,99 @@ class King : public Piece
         }
 };
 
-void show_name(Piece & p)
+class Square
 {
-    p.get_name();
-}
+    unsigned short row;
+    char col;
+ 
+    public:
+        Square(char c, unsigned short r)
+        {
+            c = tolower(c);
+            if(r > 8 || r < 1)
+                throw "Invalid row.";           
+            if(c < 'a' || c > 'h')
+                throw "Invalid column.";
+            row = r;
+            col = c;
+        }
+        void print()
+        {
+            cout << col << row;
+        }
+};
+
+class Chessboard
+{
+    Piece * board[BOARD_SIZE][BOARD_SIZE];
+    
+    public:
+        Chessboard()
+        {
+            board[0][0] = new Rook(WHITE);
+            board[0][1] = new Knight(WHITE);
+            board[0][2] = new Bishop(WHITE);
+            board[0][3] = new Queen(WHITE);
+            board[0][4] = new King(WHITE);
+            board[0][5] = new Bishop(WHITE);
+            board[0][6] = new Knight(WHITE);
+            board[0][7] = new Rook(WHITE);
+
+            board[7][0] = new Rook(BLACK);
+            board[7][1] = new Knight(BLACK);
+            board[7][2] = new Bishop(BLACK);
+            board[7][3] = new Queen(BLACK);
+            board[7][4] = new King(BLACK);
+            board[7][5] = new Bishop(BLACK);
+            board[7][6] = new Knight(BLACK);
+            board[7][7] = new Rook(BLACK);
+            
+            for(short i = 0; i < BOARD_SIZE; i++)
+            {
+                board[1][i] = new Pawn(WHITE);
+                board[6][i] = new Pawn(BLACK);
+            }
+            
+            for(short i = 2; i < 6; i++)
+                for(short j = 0; j < BOARD_SIZE; j++)
+                    board[i][j] = NULL;
+        }
+        
+        void draw()
+        {
+            for(short i = BOARD_SIZE - 1; i >= 0; i--)
+            {
+                cout << i + 1;
+                for(short j = 0; j < BOARD_SIZE; j++)
+                    if(board[i][j] != NULL)
+                        board[i][j]->draw();
+                    else
+                        cout << ".";
+                cout << endl;
+            }
+            cout << " abcdefgh" << endl;
+        }
+};
+
+
     
 int main()
 {
-    Pawn pawn(WHITE);
-    Rook rook(BLACK);
-    Bishop bishop(BLACK);
-    Knight knight(BLACK);
-    Queen queen(BLACK);
-    King king(BLACK);
-    show_name(pawn);
-    show_name(rook);
-    show_name(bishop);
-    show_name(knight);
-    show_name(queen);
-    show_name(king);
-    king.draw();
-    queen.draw();
-    knight.draw();
-    bishop.draw();
-    pawn.draw();
-    rook.draw();
+    //Chessboard board;
+    //board.draw();
+    try
+    {
+        Square square('-', 1);
+        square.print();
+    }
+    catch (const char* exp)
+    {
+        cout << exp << endl;
+        return 1;
+    }
+    
+    
+    
     
     return 0;
 }
